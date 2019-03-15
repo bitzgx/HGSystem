d87d82f227c4e7f9b86ac3064a019b66310baea7
zhangguixin <zhangguixin@topwisesz.com>
2019-03-15 20:18:55 +0800

    add floatwindow

--- HGSystem/MainForm.cs
+++ HGSystem/MainForm.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace HGSystem
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btn_float_Click(object sender, EventArgs e)
+        {
+            FloatWindow fw = new FloatWindow();
+            fw.Show();
         }
     }
 }
