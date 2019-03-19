e3ed0bbfaf6d508fc43ce1de4c4779873009f9b6
zhangguixin <zhangguixin@topwisesz.com>
2019-03-19 14:33:50 +0800

    add HGComCategory

---
+++ HGSystem/Model/HGComCategory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HGSystem.Model
+{
+    public class HGComCategorySubItem
+    {
+        protected String label;
+        protected int value;
+        public String Label { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class HGComCategoryItem : HGComCategorySubItem
+    {
+        private HGComCategorySubItem[] children;
+        public HGComCategorySubItem[] Children { get; set; }
+    }
+
+    public class HGComCategory : HGComCategoryItem
+    {
+        private HGComCategoryItem[] children;
+        public HGComCategoryItem[] Children { get; set; }
+    }
+}
