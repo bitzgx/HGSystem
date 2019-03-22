4f6399be1e51282bc7f94b0df7d49bd850c11dfc
zhangguixin <zhangguixin@topwisesz.com>
2019-03-22 19:13:56 +0800

    refine newalbumform

--- HGSystem/UI/NewAlbumForm.Designer.cs
+++ HGSystem/UI/NewAlbumForm.Designer.cs
@@ -54,7 +54,7 @@
             // 
             this.m_lbl_caption.AutoSize = true;
             this.m_lbl_caption.Font = new System.Drawing.Font("新宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.m_lbl_caption.Location = new System.Drawing.Point(266, 10);
+            this.m_lbl_caption.Location = new System.Drawing.Point(266, 13);
             this.m_lbl_caption.Name = "m_lbl_caption";
             this.m_lbl_caption.Size = new System.Drawing.Size(76, 16);
             this.m_lbl_caption.TabIndex = 0;
@@ -64,7 +64,7 @@
             // 
             this.m_lbl_separate1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.m_lbl_separate1.ForeColor = System.Drawing.Color.Gray;
-            this.m_lbl_separate1.Location = new System.Drawing.Point(15, 38);
+            this.m_lbl_separate1.Location = new System.Drawing.Point(15, 41);
             this.m_lbl_separate1.Name = "m_lbl_separate1";
             this.m_lbl_separate1.Size = new System.Drawing.Size(562, 2);
             this.m_lbl_separate1.TabIndex = 1;
@@ -73,7 +73,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 10F);
-            this.label1.Location = new System.Drawing.Point(34, 59);
+            this.label1.Location = new System.Drawing.Point(34, 62);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 14);
             this.label1.TabIndex = 2;
@@ -82,7 +82,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("宋体", 12F);
-            this.textBox1.Location = new System.Drawing.Point(124, 53);
+            this.textBox1.Location = new System.Drawing.Point(124, 56);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(287, 26);
             this.textBox1.TabIndex = 3;
@@ -92,7 +92,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 10F);
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(25, 59);
+            this.label2.Location = new System.Drawing.Point(25, 62);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(14, 14);
             this.label2.TabIndex = 4;
@@ -101,7 +101,7 @@
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("宋体", 12F);
-            this.textBox2.Location = new System.Drawing.Point(124, 93);
+            this.textBox2.Location = new System.Drawing.Point(124, 96);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(287, 26);
             this.textBox2.TabIndex = 6;
@@ -110,7 +110,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("宋体", 10F);
-            this.label3.Location = new System.Drawing.Point(34, 99);
+            this.label3.Location = new System.Drawing.Point(34, 102);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(77, 14);
             this.label3.TabIndex = 5;
@@ -120,7 +120,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("宋体", 10F);
-            this.label4.Location = new System.Drawing.Point(34, 137);
+            this.label4.Location = new System.Drawing.Point(34, 140);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(77, 14);
             this.label4.TabIndex = 7;
@@ -130,7 +130,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("宋体", 10F);
-            this.label5.Location = new System.Drawing.Point(34, 177);
+            this.label5.Location = new System.Drawing.Point(34, 180);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(77, 14);
             this.label5.TabIndex = 9;
@@ -140,7 +140,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("宋体", 10F);
-            this.label6.Location = new System.Drawing.Point(34, 330);
+            this.label6.Location = new System.Drawing.Point(34, 333);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(77, 14);
             this.label6.TabIndex = 10;
@@ -151,7 +151,7 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("宋体", 10F);
             this.label7.ForeColor = System.Drawing.Color.Red;
-            this.label7.Location = new System.Drawing.Point(25, 138);
+            this.label7.Location = new System.Drawing.Point(25, 141);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(14, 14);
             this.label7.TabIndex = 11;
@@ -159,7 +159,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(124, 175);
+            this.textBox3.Location = new System.Drawing.Point(124, 178);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(432, 140);
@@ -169,7 +169,7 @@
             // 
             this.comboBox1.Font = new System.Drawing.Font("宋体", 12F);
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(124, 132);
+            this.comboBox1.Location = new System.Drawing.Point(124, 135);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(134, 24);
             this.comboBox1.TabIndex = 13;
@@ -178,7 +178,7 @@
             // 
             this.comboBox2.Font = new System.Drawing.Font("宋体", 12F);
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(273, 132);
+            this.comboBox2.Location = new System.Drawing.Point(273, 135);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(134, 24);
             this.comboBox2.TabIndex = 14;
@@ -187,7 +187,7 @@
             // 
             this.comboBox3.Font = new System.Drawing.Font("宋体", 12F);
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(422, 132);
+            this.comboBox3.Location = new System.Drawing.Point(422, 135);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(134, 24);
             this.comboBox3.TabIndex = 15;
@@ -195,7 +195,7 @@
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(124, 330);
+            this.pictureBox1.Location = new System.Drawing.Point(124, 333);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(180, 180);
             this.pictureBox1.TabIndex = 16;
@@ -206,9 +206,9 @@
             this.m_bte_add.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(100)))), ((int)(((byte)(92)))));
             this.m_bte_add.Font = new System.Drawing.Font("宋体", 10F);
             this.m_bte_add.ForeColor = System.Drawing.Color.White;
-            this.m_bte_add.Location = new System.Drawing.Point(347, 524);
+            this.m_bte_add.Location = new System.Drawing.Point(347, 527);
             this.m_bte_add.Name = "m_bte_add";
-            this.m_bte_add.Size = new System.Drawing.Size(75, 35);
+            this.m_bte_add.Size = new System.Drawing.Size(75, 28);
             this.m_bte_add.TabIndex = 18;
             this.m_bte_add.Text = "创建";
             this.m_bte_add.UseVisualStyleBackColor = true;
@@ -216,21 +216,23 @@
             // 
             // m_bte_cancel
             // 
-            this.m_bte_cancel.BaseColor = System.Drawing.Color.White;
+            this.m_bte_cancel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.m_bte_cancel.BaseColor = System.Drawing.Color.WhiteSmoke;
             this.m_bte_cancel.Font = new System.Drawing.Font("宋体", 10F);
             this.m_bte_cancel.ForeColor = System.Drawing.Color.Black;
-            this.m_bte_cancel.Location = new System.Drawing.Point(190, 524);
+            this.m_bte_cancel.Location = new System.Drawing.Point(190, 527);
             this.m_bte_cancel.Name = "m_bte_cancel";
-            this.m_bte_cancel.Size = new System.Drawing.Size(75, 35);
+            this.m_bte_cancel.Size = new System.Drawing.Size(75, 28);
             this.m_bte_cancel.TabIndex = 17;
             this.m_bte_cancel.Text = "取消";
-            this.m_bte_cancel.UseVisualStyleBackColor = true;
+            this.m_bte_cancel.UseVisualStyleBackColor = false;
             this.m_bte_cancel.Click += new System.EventHandler(this.m_bte_cancel_Click);
             // 
             // NewAlbumForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(592, 570);
             this.Controls.Add(this.m_bte_add);
             this.Controls.Add(this.m_bte_cancel);
