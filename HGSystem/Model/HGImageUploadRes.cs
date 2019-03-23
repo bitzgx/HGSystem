dbad89873f38233299e4a2575234ee9b157a3122
zhangguixin <zhangguixin@topwisesz.com>
2019-03-23 14:38:03 +0800

    refine image uploading in new album feature

---
+++ HGSystem/Model/HGImageUploadRes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HGSystem.Model
+{
+    public class HGImageUploadData
+    {
+        public String FileId {get; set;}
+        public String RealUrl {get; set;}
+        public String FileName {get; set;}
+        public int Duration {get; set;}
+        public int[] Wh { get; set;}
+        public String Thumbnail { get; set; }
+    }
+    public class HGImageUploadRes
+    {
+        public int Status { get; set; }
+        public String Url {get; set;}
+        public String ComputedHash {get; set;}
+        public String LocalPath {get; set;}
+        public int Size {get; set;}
+        public HGImageUploadData Data { get; set; }
+    }
+}
