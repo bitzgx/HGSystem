aa4036c4a59e76bac2f323181b3cb2ef5b7357de
bitzgx <bitzgx@163.com>
2019-03-24 23:13:54 +0800

    add table header

--- HGSystem/UserControls/ProgramPanel.Designer.cs
+++ HGSystem/UserControls/ProgramPanel.Designer.cs
@@ -44,8 +44,19 @@
             this.m_bt_operations = new XPTable.Models.ButtonColumn();
             this.m_lbl_sep1 = new System.Windows.Forms.Label();
             this.m_lbl_sep2 = new System.Windows.Forms.Label();
+            this.m_pnl_header = new System.Windows.Forms.Panel();
+            this.m_lbl_no = new System.Windows.Forms.Label();
+            this.m_lbl_name = new System.Windows.Forms.Label();
+            this.m_lbl_header_cat = new System.Windows.Forms.Label();
+            this.m_lbl_timelen = new System.Windows.Forms.Label();
+            this.m_lbl_share_times = new System.Windows.Forms.Label();
+            this.m_lbl_play_times = new System.Windows.Forms.Label();
+            this.m_lbl_updatetime = new System.Windows.Forms.Label();
+            this.m_lbl_op = new System.Windows.Forms.Label();
+            this.m_lbl_sep3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.m_pbx_cover)).BeginInit();
             this.m_pnl_albuminfo.SuspendLayout();
+            this.m_pnl_header.SuspendLayout();
             this.SuspendLayout();
             // 
             // m_pbx_cover
@@ -166,10 +177,116 @@
             this.m_lbl_sep2.Size = new System.Drawing.Size(1200, 3);
             this.m_lbl_sep2.TabIndex = 6;
             // 
+            // m_pnl_header
+            // 
+            this.m_pnl_header.Controls.Add(this.m_lbl_op);
+            this.m_pnl_header.Controls.Add(this.m_lbl_share_times);
+            this.m_pnl_header.Controls.Add(this.m_lbl_play_times);
+            this.m_pnl_header.Controls.Add(this.m_lbl_updatetime);
+            this.m_pnl_header.Controls.Add(this.m_lbl_header_cat);
+            this.m_pnl_header.Controls.Add(this.m_lbl_timelen);
+            this.m_pnl_header.Controls.Add(this.m_lbl_name);
+            this.m_pnl_header.Controls.Add(this.m_lbl_no);
+            this.m_pnl_header.Location = new System.Drawing.Point(0, 186);
+            this.m_pnl_header.Name = "m_pnl_header";
+            this.m_pnl_header.Size = new System.Drawing.Size(1200, 30);
+            this.m_pnl_header.TabIndex = 7;
+            // 
+            // m_lbl_no
+            // 
+            this.m_lbl_no.AutoSize = true;
+            this.m_lbl_no.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_no.Location = new System.Drawing.Point(38, 7);
+            this.m_lbl_no.Name = "m_lbl_no";
+            this.m_lbl_no.Size = new System.Drawing.Size(37, 15);
+            this.m_lbl_no.TabIndex = 0;
+            this.m_lbl_no.Text = "序号";
+            // 
+            // m_lbl_name
+            // 
+            this.m_lbl_name.AutoSize = true;
+            this.m_lbl_name.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_name.Location = new System.Drawing.Point(144, 7);
+            this.m_lbl_name.Name = "m_lbl_name";
+            this.m_lbl_name.Size = new System.Drawing.Size(67, 15);
+            this.m_lbl_name.TabIndex = 1;
+            this.m_lbl_name.Text = "节目名称";
+            // 
+            // m_lbl_header_cat
+            // 
+            this.m_lbl_header_cat.AutoSize = true;
+            this.m_lbl_header_cat.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_header_cat.Location = new System.Drawing.Point(373, 7);
+            this.m_lbl_header_cat.Name = "m_lbl_header_cat";
+            this.m_lbl_header_cat.Size = new System.Drawing.Size(37, 15);
+            this.m_lbl_header_cat.TabIndex = 3;
+            this.m_lbl_header_cat.Text = "类别";
+            // 
+            // m_lbl_timelen
+            // 
+            this.m_lbl_timelen.AutoSize = true;
+            this.m_lbl_timelen.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_timelen.Location = new System.Drawing.Point(291, 7);
+            this.m_lbl_timelen.Name = "m_lbl_timelen";
+            this.m_lbl_timelen.Size = new System.Drawing.Size(37, 15);
+            this.m_lbl_timelen.TabIndex = 2;
+            this.m_lbl_timelen.Text = "时长";
+            // 
+            // m_lbl_share_times
+            // 
+            this.m_lbl_share_times.AutoSize = true;
+            this.m_lbl_share_times.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_share_times.Location = new System.Drawing.Point(740, 7);
+            this.m_lbl_share_times.Name = "m_lbl_share_times";
+            this.m_lbl_share_times.Size = new System.Drawing.Size(52, 15);
+            this.m_lbl_share_times.TabIndex = 6;
+            this.m_lbl_share_times.Text = "转发量";
+            // 
+            // m_lbl_play_times
+            // 
+            this.m_lbl_play_times.AutoSize = true;
+            this.m_lbl_play_times.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_play_times.Location = new System.Drawing.Point(639, 7);
+            this.m_lbl_play_times.Name = "m_lbl_play_times";
+            this.m_lbl_play_times.Size = new System.Drawing.Size(52, 15);
+            this.m_lbl_play_times.TabIndex = 5;
+            this.m_lbl_play_times.Text = "播放量";
+            // 
+            // m_lbl_updatetime
+            // 
+            this.m_lbl_updatetime.AutoSize = true;
+            this.m_lbl_updatetime.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_updatetime.Location = new System.Drawing.Point(492, 7);
+            this.m_lbl_updatetime.Name = "m_lbl_updatetime";
+            this.m_lbl_updatetime.Size = new System.Drawing.Size(67, 15);
+            this.m_lbl_updatetime.TabIndex = 4;
+            this.m_lbl_updatetime.Text = "上传时间";
+            // 
+            // m_lbl_op
+            // 
+            this.m_lbl_op.AutoSize = true;
+            this.m_lbl_op.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_op.Location = new System.Drawing.Point(872, 7);
+            this.m_lbl_op.Name = "m_lbl_op";
+            this.m_lbl_op.Size = new System.Drawing.Size(37, 15);
+            this.m_lbl_op.TabIndex = 7;
+            this.m_lbl_op.Text = "操作";
+            // 
+            // m_lbl_sep3
+            // 
+            this.m_lbl_sep3.BackColor = System.Drawing.Color.Transparent;
+            this.m_lbl_sep3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.m_lbl_sep3.Location = new System.Drawing.Point(0, 219);
+            this.m_lbl_sep3.Name = "m_lbl_sep3";
+            this.m_lbl_sep3.Size = new System.Drawing.Size(1200, 3);
+            this.m_lbl_sep3.TabIndex = 8;
+            // 
             // ProgramPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.m_lbl_sep3);
+            this.Controls.Add(this.m_pnl_header);
             this.Controls.Add(this.m_lbl_sep2);
             this.Controls.Add(this.m_pnl_albuminfo);
             this.Enabled = false;
@@ -179,6 +296,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.m_pbx_cover)).EndInit();
             this.m_pnl_albuminfo.ResumeLayout(false);
             this.m_pnl_albuminfo.PerformLayout();
+            this.m_pnl_header.ResumeLayout(false);
+            this.m_pnl_header.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -201,5 +320,15 @@
         private XPTable.Models.ButtonColumn m_bt_operations;
         private System.Windows.Forms.Label m_lbl_sep1;
         private System.Windows.Forms.Label m_lbl_sep2;
+        private System.Windows.Forms.Panel m_pnl_header;
+        private System.Windows.Forms.Label m_lbl_no;
+        private System.Windows.Forms.Label m_lbl_name;
+        private System.Windows.Forms.Label m_lbl_header_cat;
+        private System.Windows.Forms.Label m_lbl_timelen;
+        private System.Windows.Forms.Label m_lbl_share_times;
+        private System.Windows.Forms.Label m_lbl_play_times;
+        private System.Windows.Forms.Label m_lbl_updatetime;
+        private System.Windows.Forms.Label m_lbl_op;
+        private System.Windows.Forms.Label m_lbl_sep3;
     }
 }
