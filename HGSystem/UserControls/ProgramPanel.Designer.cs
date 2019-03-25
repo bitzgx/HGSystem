bd50288d9e37732bc4b850cadf1d6ed166525bd9
zhangguixin <zhangguixin@topwisesz.com>
2019-03-25 09:17:14 +0800

    remove useless codes for xptable

--- HGSystem/UserControls/ProgramPanel.Designer.cs
+++ HGSystem/UserControls/ProgramPanel.Designer.cs
@@ -34,14 +34,6 @@
             this.m_lbl_cat = new System.Windows.Forms.Label();
             this.m_pnl_albuminfo = new System.Windows.Forms.Panel();
             this.m_lbl_albumintro = new System.Windows.Forms.Label();
-            this.m_tc_no = new XPTable.Models.TextColumn();
-            this.m_bc_name = new XPTable.Models.ButtonColumn();
-            this.m_tc_timelen = new XPTable.Models.TextColumn();
-            this.m_tc_cat = new XPTable.Models.TextColumn();
-            this.m_tc_createat = new XPTable.Models.TextColumn();
-            this.m_tc_play_times = new XPTable.Models.TextColumn();
-            this.m_tc_share_times = new XPTable.Models.TextColumn();
-            this.m_bt_operations = new XPTable.Models.ButtonColumn();
             this.m_lbl_sep1 = new System.Windows.Forms.Label();
             this.m_lbl_sep2 = new System.Windows.Forms.Label();
             this.m_pnl_header = new System.Windows.Forms.Panel();
@@ -122,43 +114,7 @@
             this.m_lbl_albumintro.TabIndex = 4;
             this.m_lbl_albumintro.Text = "简介：回不去的故乡 暖暖的心，贴近彼此 今冬无雪 年少当有为 一念起，天涯咫尺 永远不落的追忆。乡村炊烟，让人魂牵梦萦 孤岛山城，后会无期 清风不扰客，留香等人寻" +
     " 曾经 芳华当惜取 忘不掉和记不起";
-            // 
-            // m_tc_no
-            // 
-            this.m_tc_no.Editable = false;
-            this.m_tc_no.Sortable = false;
-            this.m_tc_no.Text = "序号";
-            // 
-            // m_bc_name
-            // 
-            this.m_bc_name.Text = "节目名称";
-            this.m_bc_name.Width = 175;
-            // 
-            // m_tc_timelen
-            // 
-            this.m_tc_timelen.Editable = false;
-            this.m_tc_timelen.Text = "时长";
-            this.m_tc_timelen.Width = 100;
-            // 
-            // m_tc_cat
-            // 
-            this.m_tc_cat.Text = "类别";
-            // 
-            // m_tc_createat
-            // 
-            this.m_tc_createat.Text = "上传时间";
-            // 
-            // m_tc_play_times
-            // 
-            this.m_tc_play_times.Text = "播放量";
-            // 
-            // m_tc_share_times
-            // 
-            this.m_tc_share_times.Text = "转发量";
-            // 
-            // m_bt_operations
-            // 
-            this.m_bt_operations.Text = "操作";
+
             // 
             // m_lbl_sep1
             // 
