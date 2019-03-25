03edd0903c2fa487291aeb04506a05bf913c540b
zhangguixin <zhangguixin@topwisesz.com>
2019-03-25 09:58:30 +0800

    enable ProgramPanel

--- HGSystem/UserControls/ProgramPanel.Designer.cs
+++ HGSystem/UserControls/ProgramPanel.Designer.cs
@@ -33,18 +33,18 @@
             this.m_lbl_albumlabel = new System.Windows.Forms.Label();
             this.m_lbl_cat = new System.Windows.Forms.Label();
             this.m_pnl_albuminfo = new System.Windows.Forms.Panel();
-            this.m_lbl_albumintro = new System.Windows.Forms.Label();
             this.m_lbl_sep1 = new System.Windows.Forms.Label();
+            this.m_lbl_albumintro = new System.Windows.Forms.Label();
             this.m_lbl_sep2 = new System.Windows.Forms.Label();
             this.m_pnl_header = new System.Windows.Forms.Panel();
-            this.m_lbl_no = new System.Windows.Forms.Label();
-            this.m_lbl_name = new System.Windows.Forms.Label();
-            this.m_lbl_header_cat = new System.Windows.Forms.Label();
-            this.m_lbl_timelen = new System.Windows.Forms.Label();
+            this.m_lbl_op = new System.Windows.Forms.Label();
             this.m_lbl_share_times = new System.Windows.Forms.Label();
             this.m_lbl_play_times = new System.Windows.Forms.Label();
             this.m_lbl_updatetime = new System.Windows.Forms.Label();
-            this.m_lbl_op = new System.Windows.Forms.Label();
+            this.m_lbl_header_cat = new System.Windows.Forms.Label();
+            this.m_lbl_timelen = new System.Windows.Forms.Label();
+            this.m_lbl_name = new System.Windows.Forms.Label();
+            this.m_lbl_no = new System.Windows.Forms.Label();
             this.m_lbl_sep3 = new System.Windows.Forms.Label();
             this.programItemRow1 = new HGSystem.UserControls.ProgramItemRow();
             ((System.ComponentModel.ISupportInitialize)(this.m_pbx_cover)).BeginInit();
@@ -105,6 +105,15 @@
             this.m_pnl_albuminfo.Size = new System.Drawing.Size(1200, 172);
             this.m_pnl_albuminfo.TabIndex = 5;
             // 
+            // m_lbl_sep1
+            // 
+            this.m_lbl_sep1.BackColor = System.Drawing.Color.Transparent;
+            this.m_lbl_sep1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.m_lbl_sep1.Location = new System.Drawing.Point(0, 1);
+            this.m_lbl_sep1.Name = "m_lbl_sep1";
+            this.m_lbl_sep1.Size = new System.Drawing.Size(1200, 3);
+            this.m_lbl_sep1.TabIndex = 5;
+            // 
             // m_lbl_albumintro
             // 
             this.m_lbl_albumintro.Font = new System.Drawing.Font("宋体", 11F);
@@ -114,16 +123,6 @@
             this.m_lbl_albumintro.TabIndex = 4;
             this.m_lbl_albumintro.Text = "简介：回不去的故乡 暖暖的心，贴近彼此 今冬无雪 年少当有为 一念起，天涯咫尺 永远不落的追忆。乡村炊烟，让人魂牵梦萦 孤岛山城，后会无期 清风不扰客，留香等人寻" +
     " 曾经 芳华当惜取 忘不掉和记不起";
-
-            // 
-            // m_lbl_sep1
-            // 
-            this.m_lbl_sep1.BackColor = System.Drawing.Color.Transparent;
-            this.m_lbl_sep1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.m_lbl_sep1.Location = new System.Drawing.Point(0, 1);
-            this.m_lbl_sep1.Name = "m_lbl_sep1";
-            this.m_lbl_sep1.Size = new System.Drawing.Size(1200, 3);
-            this.m_lbl_sep1.TabIndex = 5;
             // 
             // m_lbl_sep2
             // 
@@ -149,45 +148,15 @@
             this.m_pnl_header.Size = new System.Drawing.Size(1200, 30);
             this.m_pnl_header.TabIndex = 7;
             // 
-            // m_lbl_no
+            // m_lbl_op
             // 
-            this.m_lbl_no.AutoSize = true;
-            this.m_lbl_no.Font = new System.Drawing.Font("宋体", 11F);
-            this.m_lbl_no.Location = new System.Drawing.Point(38, 7);
-            this.m_lbl_no.Name = "m_lbl_no";
-            this.m_lbl_no.Size = new System.Drawing.Size(37, 15);
-            this.m_lbl_no.TabIndex = 0;
-            this.m_lbl_no.Text = "序号";
-            // 
-            // m_lbl_name
-            // 
-            this.m_lbl_name.AutoSize = true;
-            this.m_lbl_name.Font = new System.Drawing.Font("宋体", 11F);
-            this.m_lbl_name.Location = new System.Drawing.Point(152, 7);
-            this.m_lbl_name.Name = "m_lbl_name";
-            this.m_lbl_name.Size = new System.Drawing.Size(67, 15);
-            this.m_lbl_name.TabIndex = 1;
-            this.m_lbl_name.Text = "节目名称";
-            // 
-            // m_lbl_header_cat
-            // 
-            this.m_lbl_header_cat.AutoSize = true;
-            this.m_lbl_header_cat.Font = new System.Drawing.Font("宋体", 11F);
-            this.m_lbl_header_cat.Location = new System.Drawing.Point(470, 7);
-            this.m_lbl_header_cat.Name = "m_lbl_header_cat";
-            this.m_lbl_header_cat.Size = new System.Drawing.Size(37, 15);
-            this.m_lbl_header_cat.TabIndex = 3;
-            this.m_lbl_header_cat.Text = "类别";
-            // 
-            // m_lbl_timelen
-            // 
-            this.m_lbl_timelen.AutoSize = true;
-            this.m_lbl_timelen.Font = new System.Drawing.Font("宋体", 11F);
-            this.m_lbl_timelen.Location = new System.Drawing.Point(338, 7);
-            this.m_lbl_timelen.Name = "m_lbl_timelen";
-            this.m_lbl_timelen.Size = new System.Drawing.Size(37, 15);
-            this.m_lbl_timelen.TabIndex = 2;
-            this.m_lbl_timelen.Text = "时长";
+            this.m_lbl_op.AutoSize = true;
+            this.m_lbl_op.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_op.Location = new System.Drawing.Point(969, 7);
+            this.m_lbl_op.Name = "m_lbl_op";
+            this.m_lbl_op.Size = new System.Drawing.Size(37, 15);
+            this.m_lbl_op.TabIndex = 7;
+            this.m_lbl_op.Text = "操作";
             // 
             // m_lbl_share_times
             // 
@@ -219,15 +188,45 @@
             this.m_lbl_updatetime.TabIndex = 4;
             this.m_lbl_updatetime.Text = "上传时间";
             // 
-            // m_lbl_op
+            // m_lbl_header_cat
             // 
-            this.m_lbl_op.AutoSize = true;
-            this.m_lbl_op.Font = new System.Drawing.Font("宋体", 11F);
-            this.m_lbl_op.Location = new System.Drawing.Point(969, 7);
-            this.m_lbl_op.Name = "m_lbl_op";
-            this.m_lbl_op.Size = new System.Drawing.Size(37, 15);
-            this.m_lbl_op.TabIndex = 7;
-            this.m_lbl_op.Text = "操作";
+            this.m_lbl_header_cat.AutoSize = true;
+            this.m_lbl_header_cat.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_header_cat.Location = new System.Drawing.Point(470, 7);
+            this.m_lbl_header_cat.Name = "m_lbl_header_cat";
+            this.m_lbl_header_cat.Size = new System.Drawing.Size(37, 15);
+            this.m_lbl_header_cat.TabIndex = 3;
+            this.m_lbl_header_cat.Text = "类别";
+            // 
+            // m_lbl_timelen
+            // 
+            this.m_lbl_timelen.AutoSize = true;
+            this.m_lbl_timelen.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_timelen.Location = new System.Drawing.Point(338, 7);
+            this.m_lbl_timelen.Name = "m_lbl_timelen";
+            this.m_lbl_timelen.Size = new System.Drawing.Size(37, 15);
+            this.m_lbl_timelen.TabIndex = 2;
+            this.m_lbl_timelen.Text = "时长";
+            // 
+            // m_lbl_name
+            // 
+            this.m_lbl_name.AutoSize = true;
+            this.m_lbl_name.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_name.Location = new System.Drawing.Point(152, 7);
+            this.m_lbl_name.Name = "m_lbl_name";
+            this.m_lbl_name.Size = new System.Drawing.Size(67, 15);
+            this.m_lbl_name.TabIndex = 1;
+            this.m_lbl_name.Text = "节目名称";
+            // 
+            // m_lbl_no
+            // 
+            this.m_lbl_no.AutoSize = true;
+            this.m_lbl_no.Font = new System.Drawing.Font("宋体", 11F);
+            this.m_lbl_no.Location = new System.Drawing.Point(38, 7);
+            this.m_lbl_no.Name = "m_lbl_no";
+            this.m_lbl_no.Size = new System.Drawing.Size(37, 15);
+            this.m_lbl_no.TabIndex = 0;
+            this.m_lbl_no.Text = "序号";
             // 
             // m_lbl_sep3
             // 
@@ -240,6 +239,7 @@
             // 
             // programItemRow1
             // 
+            this.programItemRow1.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.programItemRow1.Font = new System.Drawing.Font("宋体", 11F);
             this.programItemRow1.Location = new System.Drawing.Point(0, 226);
             this.programItemRow1.Margin = new System.Windows.Forms.Padding(4);
@@ -256,7 +256,6 @@
             this.Controls.Add(this.m_pnl_header);
             this.Controls.Add(this.m_lbl_sep2);
             this.Controls.Add(this.m_pnl_albuminfo);
-            this.Enabled = false;
             this.Name = "ProgramPanel";
             this.Size = new System.Drawing.Size(1200, 768);
             this.Load += new System.EventHandler(this.ProgramPanel_Load);
