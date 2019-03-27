39e93ae1fbd5f37bcb7baca18f238c041a95a206
zhangguixin <zhangguixin@topwisesz.com>
2019-03-27 13:52:04 +0800

    refine settings

--- HGSystem/UserControls/UCSettings.Designer.cs
+++ HGSystem/UserControls/UCSettings.Designer.cs
@@ -28,12 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCSettings));
             this.m_lbl_transferlist = new System.Windows.Forms.Label();
             this.m_lbl_logout = new System.Windows.Forms.Label();
             this.m_lbl_resetpwd = new System.Windows.Forms.Label();
             this.m_lbl_faq = new System.Windows.Forms.Label();
             this.m_lbl_vertical_sep = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.m_lbl_uploading = new System.Windows.Forms.Label();
+            this.m_lbl_downloading = new System.Windows.Forms.Label();
+            this.m_lbl_downloaded = new System.Windows.Forms.Label();
+            this.m_lbl_tableheader = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // m_lbl_transferlist
@@ -90,11 +96,63 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.m_lbl_tableheader);
+            this.panel1.Controls.Add(this.m_lbl_downloaded);
+            this.panel1.Controls.Add(this.m_lbl_downloading);
+            this.panel1.Controls.Add(this.m_lbl_uploading);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(240, 0);
+            this.panel1.Location = new System.Drawing.Point(-90, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(556, 473);
+            this.panel1.Size = new System.Drawing.Size(886, 473);
             this.panel1.TabIndex = 5;
+            // 
+            // m_lbl_uploading
+            // 
+            this.m_lbl_uploading.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.m_lbl_uploading.Image = ((System.Drawing.Image)(resources.GetObject("m_lbl_uploading.Image")));
+            this.m_lbl_uploading.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.m_lbl_uploading.Location = new System.Drawing.Point(41, 24);
+            this.m_lbl_uploading.Name = "m_lbl_uploading";
+            this.m_lbl_uploading.Size = new System.Drawing.Size(88, 23);
+            this.m_lbl_uploading.TabIndex = 0;
+            this.m_lbl_uploading.Text = "上传中";
+            this.m_lbl_uploading.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // m_lbl_downloading
+            // 
+            this.m_lbl_downloading.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.m_lbl_downloading.Image = ((System.Drawing.Image)(resources.GetObject("m_lbl_downloading.Image")));
+            this.m_lbl_downloading.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.m_lbl_downloading.Location = new System.Drawing.Point(150, 24);
+            this.m_lbl_downloading.Name = "m_lbl_downloading";
+            this.m_lbl_downloading.Size = new System.Drawing.Size(88, 23);
+            this.m_lbl_downloading.TabIndex = 1;
+            this.m_lbl_downloading.Text = "下载中";
+            this.m_lbl_downloading.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // m_lbl_downloaded
+            // 
+            this.m_lbl_downloaded.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.m_lbl_downloaded.Image = ((System.Drawing.Image)(resources.GetObject("m_lbl_downloaded.Image")));
+            this.m_lbl_downloaded.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.m_lbl_downloaded.Location = new System.Drawing.Point(259, 24);
+            this.m_lbl_downloaded.Name = "m_lbl_downloaded";
+            this.m_lbl_downloaded.Size = new System.Drawing.Size(88, 23);
+            this.m_lbl_downloaded.TabIndex = 2;
+            this.m_lbl_downloaded.Text = "已下载";
+            this.m_lbl_downloaded.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // m_lbl_tableheader
+            // 
+            this.m_lbl_tableheader.BackColor = System.Drawing.Color.DarkGray;
+            this.m_lbl_tableheader.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.m_lbl_tableheader.Font = new System.Drawing.Font("宋体", 15F);
+            this.m_lbl_tableheader.Location = new System.Drawing.Point(0, 65);
+            this.m_lbl_tableheader.Name = "m_lbl_tableheader";
+            this.m_lbl_tableheader.Size = new System.Drawing.Size(886, 38);
+            this.m_lbl_tableheader.TabIndex = 3;
+            this.m_lbl_tableheader.Text = "文件名称                                   进度                            操作";
+            this.m_lbl_tableheader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // UCSettings
             // 
@@ -108,6 +166,7 @@
             this.Controls.Add(this.m_lbl_transferlist);
             this.Name = "UCSettings";
             this.Size = new System.Drawing.Size(796, 473);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,6 +180,10 @@
         private System.Windows.Forms.Label m_lbl_faq;
         private System.Windows.Forms.Label m_lbl_vertical_sep;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label m_lbl_uploading;
+        private System.Windows.Forms.Label m_lbl_downloading;
+        private System.Windows.Forms.Label m_lbl_downloaded;
+        private System.Windows.Forms.Label m_lbl_tableheader;
 
 
     }
